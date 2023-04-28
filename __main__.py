d34afb2ad0acd7c50778fb9823d25b560dd71bab
souravran <souravran@gmail.com>
2023-04-28 17:12:57 +0200

    - Minor correction to the landing page

--- __main__.py
+++ __main__.py
@@ -1,10 +1,7 @@
 
-from flask import Flask, request, Response, make_response, render_template
+from flask import Flask, request, render_template
 import logging
 import os
-
-import pickle
-import numpy as np
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s (%(filename)s:%(lineno)d) - [%(levelname)s] - %(message)s",
@@ -14,30 +11,15 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return render_template('index.html')
-
-# POST
-# ----------------------------------------------------------------------------------------------------------------------
-@app.route("/predict", methods=["POST"])
+@app.route("/", methods=['GET', 'POST'])
 def predict():
-    # preg = int(request.form['pregnancies'])
-    # glucose = int(request.form['glucose'])
-    # bp = int(request.form['bloodpressure'])
-    # st = int(request.form['skinthickness'])
-    # insulin = int(request.form['insulin'])
-    # bmi = float(request.form['bmi'])
-    # dpf = float(request.form['dpf'])
-    # age = int(request.form['age'])
-    
-    # data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
-    # my_prediction = classifier.predict(data)
-    my_prediction = 0.2
-    
-    logger.info(f"Here is the predicted value: {my_prediction}")
+    prediction = None
+    if request.method == 'POST':
+        # prediction = 0
+        
+        logger.info(f"Here is the predicted value: {prediction}")
 
-    return render_template('prediction.html', prediction=my_prediction)
+    return render_template('index.html', prediction=prediction)
 
 
 if __name__ == '__main__':
