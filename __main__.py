9c28274e4c37af6a260b4c1e0f3cb21e257e20a8
souravran <souravran@gmail.com>
2023-05-04 00:00:11 +0200

    - Update the app and Heroku deply meta files

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 
 from flask import Flask, request, render_template
+import numpy as np
+import pickle
 import logging
 import os
 
@@ -9,15 +11,32 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+# load prediction model
+model_file = 'model/model.pkl'
+predictor = pickle.load(open(model_file, 'rb'))
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def predict():
     prediction = None
     if request.method == 'POST':
-        # prediction = 0
+        try:
+            pregnancies = int(request.form['pregnancies'])
+            glucose = int(request.form['glucose'])
+            bp = int(request.form['bloodpressure'])
+            skin = int(request.form['skinthickness'])
+            insulin = int(request.form['insulin'])
+            bmi = float(request.form['bmi'])
+            dpf = float(request.form['dpf'])
+            age = int(request.form['age'])
+            
+            input = np.array([[pregnancies, glucose, bp, skin, insulin, bmi, dpf, age]])
+            prediction = predictor.predict(input)
         
-        logger.info(f"Here is the predicted value: {prediction}")
+            logger.info(f"Here is the predicted value: {prediction}")
+        except ValueError:
+            logger.info("Invalid integer value supplied.")
 
     return render_template('index.html', prediction=prediction)
 
